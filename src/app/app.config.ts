3a589441af17796d3544cbce77ad5ab08bf1b14d
tk2rush90 <tk2rush90@naver.com>
2025-03-11 20:16:11 +0900

    chore(기본세팅): 기본 폰트 및 앱 기본 구조 설정

---
+++ src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig } from '@angular/core';
+import {
+  PreloadAllModules,
+  provideRouter,
+  RouteReuseStrategy,
+  withPreloading,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideClientHydration(withEventReplay()),
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
-import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
-import { routes } from './app/app.routes';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)), provideClientHydration(withEventReplay()),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig);
