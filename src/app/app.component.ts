dc55f8a61c251487441b4af81ce39280aa37185e
tk2rush90 <tk2rush90@naver.com>
2025-03-12 11:32:53 +0900

    feat(컴포넌트): 라디오/체크박스 버튼 기본 스타일 변경

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
-import { CheckboxComponent } from './components/common/checkbox/checkbox.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, FormsModule, IonRouterOutlet, CheckboxComponent],
+  imports: [IonApp, FormsModule, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() {}
