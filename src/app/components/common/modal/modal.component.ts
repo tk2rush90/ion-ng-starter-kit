0758262a10a879cedc4f0911082ca17d4fe78680
tk2rush90 <tk2rush90@naver.com>
2025-05-09 12:14:59 +0900

    feat: IconButtonDirective 삭제 및 Modal 컴포넌트 수정

--- src/app/components/common/modal/modal.component.ts
+++ src/app/components/common/modal/modal.component.ts
@@ -1,30 +1,56 @@
-import { booleanAttribute, Component, Inject, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  booleanAttribute,
+  Component,
+  ElementRef,
+  HostListener,
+  Inject,
+  Input,
+  OnDestroy,
+} from '@angular/core';
 import { OverlayRef } from '../../../services/app/overlay/overlay.service';
-import { IconButtonDirective } from '../icon-button/icon-button.directive';
 import { IconXMarkComponent } from '../../icons/icon-x-mark/icon-x-mark.component';
 import { OVERLAY_REF } from '../../../tokens/overlay-ref';
 
 /** A modal container */
 @Component({
   selector: 'app-modal',
-  imports: [IconButtonDirective, IconXMarkComponent],
+  imports: [IconXMarkComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
   host: {
-    class:
-      'fixed bg-white rounded-2xl p-6 flex-col-stretch gap-5 shadow-lg top-1/2 left-1/2 pointer-events-auto max-h-[calc(100dvh-32px)] overflow-auto w-[calc(100dvw-32px)]',
-    // To bind `transform` animation or TailwindCSS classes, use `translate` attribute.
+    // Angular 애니메이션 중 transform 속성에 영향을 미치는 애니메이션 정상적으로 표시하기 위함
     '[style.translate]': `'-50% -50%'`,
   },
 })
-export class ModalComponent {
+export class ModalComponent implements AfterViewInit, OnDestroy {
   /** Set modal title */
   @Input() modalTitle = '';
 
   /** Set to display close button */
   @Input({ transform: booleanAttribute }) displayClose = false;
 
-  constructor(@Inject(OVERLAY_REF) private readonly overlayRef: OverlayRef) {}
+  isScrolled = false;
+
+  private scrollDetectTimeout: any;
+
+  constructor(
+    @Inject(OVERLAY_REF) private readonly overlayRef: OverlayRef,
+    private readonly elementRef: ElementRef<HTMLElement>,
+  ) {}
+
+  ngAfterViewInit() {
+    this.scrollDetectTimeout = setTimeout(() => this.detectScroll());
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.scrollDetectTimeout);
+  }
+
+  @HostListener('scroll')
+  detectScroll(): void {
+    this.isScrolled = this.elementRef.nativeElement.scrollTop > 0;
+  }
 
   /** Close modal */
   close(): void {
